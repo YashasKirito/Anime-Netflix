c6260388d9bbc0f85c4d5917f76b34a52bb99c47
YashasKirito <14ps7059@gmail.com>
2022-10-06 00:47:38 +0530

    Changed the watch page to client side render

--- pages/anime/watch/[watchId].tsx
+++ pages/anime/watch/[watchId].tsx
@@ -1,17 +1,56 @@
-import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
-import React from 'react'
-import { urls } from 'service/urls';
-import axios from 'axios';
+import { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import { urls } from "service/urls";
+import axios from "axios";
 import dynamic from "next/dynamic";
+import Spinner from "@atoms/Spinner";
+import { useRouter } from "next/router";
 
 const ReactPlayer = dynamic(() => import("react-player"), {
   ssr: false,
 });
 
-const Player: NextPage = ({
-  streamData,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  let stream = streamData.sources.find((st: any) => st.quality === 'default')
+const Player: NextPage = () => {
+  const router = useRouter();
+  const [streamData, setStreamData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getStream = async () => {
+      try {
+        const res = await axios.get(
+          process.env.NEXT_PUBLIC_BASE_URL +
+            urls.watch +
+            router.query?.watchId || ""
+        );
+        setStreamData(res.data);
+        setError(null);
+      } catch (e) {
+        setError("Some error");
+      }
+    };
+    getStream();
+  }, [router.query.watchId]);
+
+  if (error) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="font-bold text-4xl">
+          Sorry couldn&apos;t get the episode, please try again later!
+        </h1>
+      </div>
+    );
+  }
+
+  if (!streamData) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  let stream = streamData.sources.find((st: any) => st.quality === "default");
 
   if (!stream) {
     stream = streamData.sources[0];
@@ -34,17 +73,4 @@ const Player: NextPage = ({
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await axios.get(
-    process.env.BASE_URL + urls.watch + context.params?.watchId || ""
-  );
-
-  console.log(res);
-  return {
-    props: {
-      streamData: res.data,
-    },
-  };
-};
-
-export default Player
+export default Player;
