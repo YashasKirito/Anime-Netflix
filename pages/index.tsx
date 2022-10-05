ae2fcca05b1ad74009e1511fc0beb910d14424c8
YashasKirito <14ps7059@gmail.com>
2022-10-05 13:45:54 +0530

    Setup server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import { GoUnmute, GoMute } from "react-icons/go";
@@ -12,7 +13,9 @@ const ReactPlayer = dynamic(() => import("react-player"), {
   ssr: false,
 });
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
   const [muted, setMuted] = useState(true);
   const [playing, setPlaying] = useState(false);
 
@@ -38,7 +41,7 @@ const HomePage: NextPage = () => {
     <div className="flex flex-col">
       <div className="h-[80vh] relative w-full flex">
         <ReactPlayer
-          url="https://www.youtube.com/watch?v=JtqIas3bYhg"
+          url={props.youtube_url}
           className="react-player"
           playing={playing}
           muted={muted}
@@ -54,17 +57,17 @@ const HomePage: NextPage = () => {
               "h-full w-full transition-opacity ease-in duration-500 object-cover",
               { "opacity-0": playing }
             )}
-            src={"/covers/EdgeRunner_cover.jpg"}
+            src={props.cover_url}
             alt="cover"
           />
-          <div className="main | absolute pl-20 bottom-0 h-full w-full bg-gradient-to-tr from-black flex flex-col-reverse">
+          <div className="main | absolute pl-20 bottom-0 h-full w-full bg-gradient-to-tr from-black  flex flex-col-reverse">
             <div className="">
               <img
                 className={cn(
                   "h-20 w-fit scale-150 duration-1000 -translate-y-10 origin-bottom-left transition-transform",
                   { "scale-100 translate-y-0": playing }
                 )}
-                src="/logos/Cyberpunk_Edgerunners_logo.png"
+                src={props.logo_url}
                 alt="Title"
               />
               <div className="flex gap-4 mt-10">
@@ -73,20 +76,16 @@ const HomePage: NextPage = () => {
                 </Button>
 
                 <Button type="secondary" onClick={() => console.log("Click")}>
-                  <BsPlusLg className="w-5 h-5" /> My Lists
+                  <BsPlusLg className="w-5 h-5" /> My List
                 </Button>
               </div>
               <p
                 className={cn(
-                  "py-8 w-1/2 text-sm text-slate-300 transition-opacity",
+                  "py-8 w-1/2 text-sm text-slate-300 transition-opacity duration-500",
                   { "opacity-0": playing }
                 )}
               >
-                CYBERPUNK: EDGERUNNERS tells a standalone, 10-episode story
-                about a street kid trying to survive in a technology and body
-                modification-obsessed city of the future. Having everything to
-                lose, he chooses to stay alive by becoming an edgerunner—a
-                mercenary outlaw also known as a cyberpunk.
+                {props.description}
               </p>
             </div>
           </div>
@@ -103,6 +102,39 @@ const HomePage: NextPage = () => {
       </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const HOME_TILES = [
+    {
+      name: "Edge runners",
+      description:
+        "CYBERPUNK: EDGERUNNERS tells a standalone, 10-episode story about a street kid trying to survive in a technology and body modification-obsessed city of the future. Having everything to lose, he chooses to stay alive by becoming an edgerunner—a mercenary outlaw also known as a cyberpunk.",
+      youtube_url: "https://www.youtube.com/watch?v=JtqIas3bYhg",
+      cover_url: "/covers/EdgeRunner_cover.jpg",
+      logo_url: "/logos/Cyberpunk_Edgerunners_logo.png",
+    },
+    {
+      name: "Yofukashi no Uta",
+      description:
+        "Wracked by insomnia and wanderlust, Kou Yamori is driven onto the moonlit streets every night in an aimless search for something he can’t seem to name. His nightly ritual is marked by purposeless introspection — until he meets Nazuna, who might just be a vampire! Kou’s new companion could offer him dark gifts and a vampire’s immortality. But there are conditions that must be met before Kou can sink his teeth into vampirism, and he’ll have to discover just how far he’s willing to go to satisfy his desires before he can heed the Call of the Night!",
+      youtube_url: "https://www.youtube.com/watch?v=ukO-ZdWS3j8",
+      cover_url: "/covers/call-of-the-night-cover.jpg",
+      logo_url: "/logos/Call_of_the_night_logo.png",
+    },
+    {
+      name: "Summer Time Render",
+      description:
+        "A sci-fi, summer story filled with suspense set on a small island with Shinpei Aijiro, whose childhood friend Ushio Kofune died. He returns to his hometown for the first time in two years for the funeral. Sou Hishigata, his best friend, suspects something's off with Ushio's death, and that someone can die next.A sinister omen is heard as an entire family next door suddenly disappears the following day. Furthermore, Mio implicates a \"shadow\" three days before Ushio's death.",
+      youtube_url: "https://www.youtube.com/watch?v=_JgOMtS2FkE",
+      cover_url: "https://images6.alphacoders.com/118/1182991.jpg",
+      logo_url:
+        "https://upload.wikimedia.org/wikipedia/commons/6/69/Summer_Time_Rendering_logo.png",
+    },
+  ];
+  return {
+    props: HOME_TILES[Math.floor(Math.random() * HOME_TILES.length)], // will be passed to the page component as props
+  };
 };
 
 export default HomePage;
