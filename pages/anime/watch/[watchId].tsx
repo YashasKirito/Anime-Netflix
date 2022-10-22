233f8be089c32c08a5c3501caccceafd9c4f8aef
YashasKirito <14ps7059@gmail.com>
2022-10-22 17:41:23 +0530

    watch page initial fix

--- pages/anime/watch/[watchId].tsx
+++ pages/anime/watch/[watchId].tsx
@@ -1,25 +1,24 @@
-import {
-  InferGetStaticPropsType,
-  NextPage,
-  GetStaticProps,
-  GetStaticPaths,
-} from "next";
-import React, { useState } from "react";
+import { NextPage } from "next";
 import { urls } from "service/urls";
 import axios from "axios";
-import dynamic from "next/dynamic";
-import Spinner from "@atoms/Spinner";
+
 import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+import CustomPlayer from "@atoms/CustomPlayer";
 
-const ReactPlayer = dynamic(() => import("react-player"), {
-  ssr: false,
-});
-
-const Player: NextPage = ({
-  streamData,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Player: NextPage = () => {
   const router = useRouter();
-  const [error, setError] = useState<string | null>(null);
+  const {
+    isLoading,
+    error,
+    data: streamData,
+  } = useQuery(["episode", router.query?.watchId], async () => {
+    const res = await axios.get(
+      process.env.NEXT_PUBLIC_BASE_URL + urls.watch + router.query?.watchId ||
+        ""
+    );
+    return res.data;
+  });
 
   if (error) {
     return (
@@ -31,60 +30,36 @@ const Player: NextPage = ({
     );
   }
 
-  if (router.isFallback) {
+  if (isLoading) {
     return (
       <div className="flex h-screen items-center justify-center">
-        <Spinner />
+        <div
+          role="status"
+          className="flex cursor-progress justify-center items-center h-56 aspect-video rounded-lg animate-pulse bg-gray-700"
+        >
+          <svg
+            className="w-12 h-12 text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 384 512"
+          >
+            <path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
       </div>
     );
   }
 
-  return streamData ? (
+  return (
     <div className="h-screen w-screen">
-      <ReactPlayer
-        url={
-          (
-            streamData.sources.find((st: any) => st.quality === "default") ||
-            streamData.sources[0]
-          ).url
-        }
-        className="react-player"
-        playing
-        width="100%"
-        height="100%"
-        volume={0.8}
-        onEnded={() => console.log("Video Ended")}
-        controls
-        // onProgress={HandleTransition}
+      <CustomPlayer
+        episodeTitle={`${router.query?.watchId}`}
+        url={streamData.sources.find((s: any) => s.quality === "default").url}
       />
     </div>
-  ) : null;
+  );
 };
 
 export default Player;
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: true,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  try {
-    const res = await axios.get(
-      process.env.BASE_URL + urls.watch + context.params?.watchId || ""
-    );
-    return {
-      props: {
-        streamData: res.data,
-      },
-      revalidate: 30,
-    };
-  } catch {
-    return {
-      notFound: true,
-      revalidate: 1,
-    };
-  }
-};
